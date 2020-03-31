8d0465a3f90c1c446a62acd64c3e74917269d4a7
sammiriche <sammiriche@163.com>
2020-04-01 00:33:52 +0800

    home0331

--- .vscode/exercise3.py
+++ .vscode/exercise3.py
@@ -1,24 +1,16 @@
-string = '   hello python.we are the world!    '
-# print(string[4])
-# for i in string:
-#     print(i,end = '')
-# t = string.count('lc')
-# print(t)
-# t = string.index('pc')
-# print(t)
-# string.
-# string = string.strip()
-# print(string)
-# string = '\n\r\t'
-# print(string.isspace())
-# string = '12'
-# print(string.isdecimal())
-# print(string.isdigit())
-# print(string.isnumeric())
-# print(string.endswith(''))
-# print(string.find('qq'))
-# print(string.index('qq'))
-t = string.join(['1','2','3'])
-print(t)
+# print('12345')
+# class test(object):
+#     print('class')
 
-print(string+'usa,america')
+print("include module")
+
+name = "jeck"
+
+def func():
+    print("hello world")
+    
+
+class test():
+    def func(self):
+        print("class hello world")
+    print('class variable')
